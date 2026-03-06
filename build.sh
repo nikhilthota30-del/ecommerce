6905fb4afc428577a10444641c5287293b9eb6cf
nikhil thota <nikhilthota20@gmail.com>
2026-03-06 13:19:45 +0530

    Triggering superuser creation build

--- build.sh
+++ build.sh
@@ -11,3 +11,16 @@ pip install -r requirements.txt
 # Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+
+# Create superuser if it doesn't exist
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'YourPassword123')
+    print("Superuser created!")
+else:
+    print("Superuser already exists.")
+END
