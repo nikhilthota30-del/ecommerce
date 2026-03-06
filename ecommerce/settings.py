b36f4eb958304d8672f0afd7349ec18688ea37a1
nikhil thota <nikhilthota20@gmail.com>
2026-03-06 14:09:36 +0530

    Fix typo in INSTALLED_APPS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', 
-    'mystore.apps.Myst6oreConfig',
+     'mystore.apps.MystoreConfig',
 ]
 
 MIDDLEWARE = [
