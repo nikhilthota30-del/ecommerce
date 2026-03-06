6a3f83b3517adf3b7daffd75342d24fb78e885cf
nikhil thota <nikhilthota20@gmail.com>
2026-03-06 13:09:31 +0530

    Add custom command to ensure admin creation

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -15,3 +15,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
 application = get_wsgi_application()
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='nikhil').exists():
+    User.objects.create_superuser('nikhil', 'nikhilthota20@gmail.com', 'nikhil09')
